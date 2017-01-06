28f4642ccd01130119eac02e0c69e9b799f78229
nemimccarter <nemimccarter@gmail.com>
2017-01-05 17:31:29 -0800

    basic graph of extent data via matplotlib completed

--- seaice.py
+++ seaice.py
@@ -1,8 +1,16 @@
 import csv
+import matplotlib.pyplot as plt
 
 seaice_file = open('seaice.csv')
 file_reader = csv.reader(seaice_file) # reader object
 seaice_data = list(file_reader) # dictionary of csv data
+years = []
+extents = []
 #format: seaice_data[row][col]
+#extract each extent and year taken
 for row in seaice_data:
-    print(row[0] + ' ' + row[3]) #prints year and extent
+    years.append(row[0])
+    extents.append(row[3])
+plt.plot(extents)
+plt.ylabel('Extent')
+plt.show()
