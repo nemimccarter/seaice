d871b45933ad18311298c6334a7a43ca520c58e5
nemimccarter <nemimccarter@gmail.com>
2017-01-05 20:02:52 -0800

    implemented dictionary instead of lists. Graph is still suspicious

--- seaice.py
+++ seaice.py
@@ -1,34 +1,33 @@
-import csv
-import matplotlib.pyplot as plt
+import csv 
+import matplotlib.pyplot as plt 
 
 seaice_file = open('seaice.csv')
 file_reader = csv.reader(seaice_file) # reader object
 seaice_data = list(file_reader) # dictionary of csv data
-years = []
-extents = []
-extent_yearly_avg = []
-#format: seaice_data[row][col]
-#extract each extent and year taken
-for row in seaice_data:
-    years.append(row[0])
-    extents.append(row[3])
+
+extent_yearly_avg = 0
+yearly_avg_dict = {} # [[year, avg_extent]] for graph
+current_year = 0 # for comparison
+yearly_sum = 0 # for averaging
+extent_sum = 0 # for averaging
+
+# set the current year to first year in csv (csv is chronological)
+if (current_year == 0):
+    current_year = seaice_data[0][0]
+
 #calculate yearly average of extent
-current_year = years[0]
-extent_yearly_sum = 0 # to be averaged
-year_count = 0        # to be averaged
-for index in range(len(years)):
-    if (years[index] == current_year):
-        extent_yearly_sum = extent_yearly_sum + float(extents[index])
-        year_count = year_count + 1
+# index method: for index in range(len(seaice_data)):
+for entry in seaice_data:
+    if (entry[0] == current_year):
+        yearly_sum = yearly_sum + 1
+        extent_sum = extent_sum + float(entry[3])
     else:
-        # we've added up all the measurements for this year
-        #calculate year's average extent
-        extent_yearly_avg.append(extent_yearly_sum / year_count)
-        # zero out counters
-        year_count = 0
-        extent_yearly_sum = 0
-        # update index and current year
-        current_year = years[index]
-plt.plot(extent_yearly_avg)
-plt.ylabel('Average Annual Extent')
+        # year has been totaled. clear/update counters
+        extent_yearly_avg = extent_sum / yearly_sum
+        yearly_avg_dict[current_year] = extent_yearly_avg
+        yearly_sum = 0
+        extent_sum = 0
+        current_year = entry[0]
+plt.plot(yearly_avg_dict.values())
+plt.ylabel("Average Extent")
 plt.show()
